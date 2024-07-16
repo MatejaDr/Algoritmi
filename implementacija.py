bd7ab424feadc7ee0e265ff6727dd66130d68d27
MatejaDr <mateja.dragicevic@dev.telent.rs>
2024-07-16 10:39:16 +0200

    Uparivanje zagrada
    Uradjen zadatak za uparivanje zagrada putem stacka

--- implementacija.py
+++ implementacija.py
@@ -5,6 +5,7 @@ class ArrayStack:
     def __len__(self):
         return len(self.data)
     
+    @property
     def is_empty(self):
         return len(self.data) == 0
     
@@ -12,12 +13,29 @@ class ArrayStack:
         self.data.append(e)
 
     def top(self):
-        if self.is_empty():
+        if self.is_empty:
             raise Exception("Stack is empty")
         return self.data[-1]
     
     def pop(self):
-        if self.is_empty():
+        if self.is_empty:
             raise Exception("Stack is empty")
         return self.data.pop()
+    
+def isMatched(exp):
+    lefty = '({['
+    righty = ')}]'
+    s = ArrayStack()
+    for c in exp:
+        if c in lefty:
+            s.push(c)
+        elif c in righty:
+            if s.is_empty:
+                return False
+            if righty.index(c) != lefty.index(s.pop()):
+                return False
+    return s.is_empty
+
+izraz = '(11-2+3+(2*2))'
+print(isMatched(izraz))
     
