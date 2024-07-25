19180442a4fae2101e0073903f2e1324d230fa63
MatejaDr <mateja.dragicevic@dev.telent.rs>
2024-07-25 13:28:32 +0200

    Update DoublyLinkedList
    (KAN-12) fix (DoublyLinkedList): head and tail are now of class node, not a print in terminal

--- DoublyLinkedList.py
+++ DoublyLinkedList.py
@@ -1,28 +1,30 @@
 class Node:
-    def __init__(self, value):
+    def __init__(self, value=None):
         self.value = value
         self.next = None
         self.prev = None
 
 class DoublyLinkedList:
     def __init__(self):
-        self.head = None
-        self.tail = None
+        self.head = Node(None)
+        self.tail = Node(None)
+        self.head.next = self.tail
+        self.tail.prev = self.head
         self.size = 0
     
     def __len__(self):
         return self.size
 
     def clear(self):
-        self.head = None
-        self.tail = None
+        self.head = Node(None)
+        self.tail = self.head
         self.size = 0
 
     def is_empty(self):
         return self.head == None
 
     def get_element(self, index):
-        current = self.head
+        current = self.head.next
         count = 0
         while current:
             if count == index:
@@ -32,13 +34,17 @@ class DoublyLinkedList:
 
     def append(self, value):
         new_node = Node(value)
-        if not self.head:
-            self.head = new_node
-            self.tail = new_node
+        if not self.head.next:
+            self.head.next = new_node
+            new_node.prev = self.head
+            self.tail.prev = new_node
+            new_node.next = self.tail
         else:
-            new_node.prev = self.tail
-            self.tail.next = new_node
-            self.tail = new_node
+            new_node.prev = self.tail.prev
+            self.tail.prev.next = new_node
+            new_node.next = self.tail
+            self.tail.prev = new_node
+        self.size += 1
 
     def get_first(self):
         if self.is_empty():
@@ -52,25 +58,29 @@ class DoublyLinkedList:
 
     def add_first(self, value):
         new_node = Node(value)
-        if not self.head:
-            self.head = new_node
-            self.tail = new_node
+        if not self.head.next:
+            self.head.next = new_node
+            new_node.prev = self.head
+            self.tail.prev = new_node
+            new_node.next = self.tail
         else:
-            new_node.next = self.head
-            self.head.prev = new_node
-            self.head = new_node
+            new_node.next = self.head.next
+            self.head.next.prev = new_node
+            new_node.prev = self.head
+            self.head.next = new_node
+        self.size += 1
 
     def insert_at(self, value, index):
         new_node = Node(value)
         if self.is_empty():
-            self.head = new_node
+            self.head.next = new_node
             self.tail = new_node
         elif index == 0:
-            new_node.next = self.head
-            self.head.prev = new_node
-            self.head = new_node
+            new_node.next = self.head.next
+            self.head.next.prev = new_node
+            self.head.next = new_node
         else:
-            current = self.head
+            current = self.head.next
             count = 0
             while current:
                 if count == index-1:
@@ -84,6 +94,7 @@ class DoublyLinkedList:
                     break
                 count += 1
                 current = current.next
+        self.size += 1
 
     def remove_first(self):
         if self.is_empty():
@@ -91,14 +102,14 @@ class DoublyLinkedList:
         else:
             to_remove = self.head.next
             if self.size == 1:
-                self.head.next = self.tail
-                self.tail.prev = self.head
+                self.head.next = None
+                self.tail = self.head
             else:
                 new_first = self.head.next.next
                 new_first.prev = self.head
                 self.head.next = new_first
             self.size -= 1
-            return to_remove
+            return to_remove.value
 
     def remove_last(self):
         if self.is_empty():
@@ -109,11 +120,11 @@ class DoublyLinkedList:
                 self.head.next = self.tail
                 self.tail.prev = self.head
             else:
-                new_last = self.tail.prev.prev
+                new_last = to_remove.prev
                 new_last.next = self.tail
                 self.tail.prev = new_last
             self.size -= 1
-            return to_remove
+            return to_remove.value
 
     def remove_at(self, index):
         if self.is_empty():
@@ -124,21 +135,24 @@ class DoublyLinkedList:
             elif index == self.size-1:
                 return self.remove_last()
             else:
-                current = self.head
+                current = self.head.next
                 count = 0
                 while current:
                     if count == index-1:
                         to_remove = current.next
                         next_node = current.next.next
                         current.next = next_node
-                        next_node.prev = current
+                        if next_node:
+                            next_node.prev = current
+                        else:
+                            self.tail = current
                         self.size -= 1
                         return to_remove.value
                     count += 1
                     current = current.next
 
     def update(self, index, new_value):
-        current = self.head
+        current = self.head.next
         count = 0
         while current:
             if count == index:
@@ -148,12 +162,11 @@ class DoublyLinkedList:
             current = current.next
 
     def display(self):
-        print("None", end=" <-> ")
         current = self.head
         while current:
             print(current.value, end=" <-> ")
             current = current.next
-        print("None")
+        print()
 
 class EmptyList(Exception):
     pass
@@ -163,20 +176,37 @@ class IndexError(Exception):
 
 def TestCode():
     dll = DoublyLinkedList()
-    dll.append(1)
-    dll.append(2)
-    dll.append(3)
+    dll.append(15)
+    dll.append(25)
+    dll.append(35)
+    print(dll.get_element(3))
     dll.add_first(7)
-    dll.insert_at(8, 2)
+    dll.update(2, 100)
+    dll.update(3, 200)
+    print(dll.get_element(0))
+    dll.remove_at(1)
+    dll.display()
+    dll.insert_at(99, 1)
+    dll.remove_first()
+    dll.display()
+    dll.add_first(70)
     print(dll.get_element(0))
     dll.display()
-    print(dll.get_first())
-    print(dll.get_last())
-    dll.remove_at(2)
-    dll.display()
-    dll.update(3, 9)
+    dll.append(300)
     dll.display()
     dll.clear()
     dll.display()
+
+def TestCodeVTWO():
+    dll = DoublyLinkedList()
+    dll.append(15)
+    dll.append(25)
+    dll.append(35)
+    dll.display()
+    dll.remove_last()
+    dll.display()
+    dll.append(30)
+    dll.display()
+    
 
 TestCode()
